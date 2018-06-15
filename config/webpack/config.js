6be66a8f4eb21d6880ac2ed7f8a06c8c7059f321
andriy.kudrya <andriy-kudrya@users.noreply.github.com>
2018-06-16 01:49:06 +0300

    switch to release version of core-js

--- config/webpack/config.js
+++ config/webpack/config.js
@@ -7,12 +7,12 @@ const config = {
     context: paths.app,
     entry: {
         app: [
-            'core-js/es/array',
-            'core-js/es/object',
-            'core-js/es/promise',
-            'core-js/es/string',
-            'core-js/es/map',
-            'core-js/es/set',
+            'core-js/es6/array',
+            'core-js/es6/object',
+            'core-js/es6/promise',
+            'core-js/es6/string',
+            'core-js/es6/map',
+            'core-js/es6/set',
 
             './bootstrap/main'
         ]
