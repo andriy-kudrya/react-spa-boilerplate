11214bafff314768c00fdbe80ebba6d29e6396e0
andriy.kudrya <andriy-kudrya@users.noreply.github.com>
2018-06-16 10:34:54 +0300

    fixed mistaken usage of type '{}' instead of 'object'

--- app/utils/object.ts
+++ app/utils/object.ts
@@ -2,12 +2,12 @@
  * Overwrites and adds fields to Target from Augmentation
  * and removes specific fields from Drop
  */
-type Redefine<Target extends {}, Augmentation extends {}, Drop extends keyof Target = never> =
+type Redefine<Target extends object, Augmentation extends object, Drop extends keyof Target = never> =
     Augmentation & Pick<Target, Exclude<keyof Target, keyof Augmentation | Drop>>
 
-type DropFields<T extends {}, K extends string | number | symbol> = Pick<T, Exclude<keyof T, K>>
+type DropFields<T extends object, K extends string | number | symbol> = Pick<T, Exclude<keyof T, K>>
 
-function dropFields<T extends {}, F extends keyof T>(target: T, ...fields: F[]): DropFields<T, F> {
+function dropFields<T extends object, F extends keyof T>(target: T, ...fields: F[]): DropFields<T, F> {
     const result = Object.assign({}, target)
     fields.forEach(_ => delete result[_])
     return result
