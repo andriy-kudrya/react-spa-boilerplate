2c1c001ff6ee239ac8e73fdb800d5e05a89c0401
andriy.kudrya <andriy-kudrya@users.noreply.github.com>
2019-11-30 16:54:28 +0200

    fixed edge case for validated input

--- app/components/html/validated-input.tsx
+++ app/components/html/validated-input.tsx
@@ -2,44 +2,68 @@ import { React } from '#/facade/react'
 import { useRef } from '#/facade/hooks'
 import { omit } from '#/utils/object'
 
-type ValidatedValue = string | ValidityState
+interface InvalidState extends Omit<ValidityState, 'valid'> {}
+
+type ValidatedValue = string | InvalidState
 
 interface Props extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
     value?: ValidatedValue
     onChange?: (value: ValidatedValue, event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
+const ValidatedInput = React.forwardRef(
+    function ValidatedInput(props: Props, ref: React.Ref<HTMLInputElement>) {
+        const lastValueRef = useRef<{ forward: ValidatedValue, target: string }>({ forward: '', target: '' })
+            , forwardProps = omit(props, 'value', 'onChange', 'onInput')
+            , value = props.value === undefined || typeof props.value === 'string'
+                ? props.value
+                : props.value === lastValueRef.current.forward
+                    ? lastValueRef.current.target
+                    : ''
 
-function ValidatedInput(props: Props, ref: React.Ref<HTMLInputElement>) {
-    const lastValueRef = useRef<{ forward: ValidatedValue, target: string }>({ forward: '', target: '' })
-        , forwardProps = omit(props, 'value', 'onChange')
-        , value = props.value === undefined || typeof props.value === 'string'
-            ? props.value
-            : props.value === lastValueRef.current.forward
-                ? lastValueRef.current.target
-                : ''
+        return <input ref={ref} {...forwardProps} onInput={handleInput} value={value}/>
 
-    return <input ref={ref} {...forwardProps} onChange={handleChange} value={value}/>
+        // Cannot handle validity change in onChange handler when first entered value is invalid.
+        // So onInput is used for invalid states as a more reliable option.
+        // E.g. there is number input, it is empty, then user type 'e' into it.
+        // In that case onChange doesn't fire.
+        function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+            if (props.onInput)
+                props.onInput(event)
 
-    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const target = event.target
-            , validity = planeValidityState(target.validity)
-            , forwardValue = validity.valid ? target.value : validity
+            const target = event.target
+                , forwardValue = target.validity.valid ? target.value : planeValidityState(target.validity)
 
-        lastValueRef.current = {
-            forward: forwardValue,
-            target: target.value,
+            lastValueRef.current = {
+                forward: forwardValue,
+                target: target.value,
+            }
+
+            if (props.onChange)
+                props.onChange(forwardValue, event)
         }
-
-        if (props.onChange)
-            props.onChange(forwardValue, event)
     }
+)
+
+function isInvalidState(val: ValidatedValue): val is InvalidState {
+    if (!(val instanceof Object))
+        return false
+
+    return 'badInput' in val
+        && 'customError' in val
+        && 'patternMismatch' in val
+        && 'rangeOverflow' in val
+        && 'rangeUnderflow' in val
+        && 'stepMismatch' in val
+        && 'tooLong' in val
+        && 'tooShort' in val
+        && 'typeMismatch' in val
+        && 'valueMissing' in val
 }
 
-export default React.forwardRef(ValidatedInput)
-export { ValidatedValue }
+export { ValidatedInput, ValidatedValue, isInvalidState }
 
-function planeValidityState(validity: ValidityState): ValidityState {
+function planeValidityState(validity: ValidityState): InvalidState {
     return {
         badInput: validity.badInput,
         customError: validity.customError,
@@ -50,7 +74,6 @@ function planeValidityState(validity: ValidityState): ValidityState {
         tooLong: validity.tooLong,
         tooShort: validity.tooShort,
         typeMismatch: validity.typeMismatch,
-        valid: validity.valid,
         valueMissing: validity.valueMissing,
     }
 }
