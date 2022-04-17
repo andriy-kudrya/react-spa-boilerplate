fb88730cfc8a17d2ad1b34667adc743bd7c935d9
andriy-kudrya <andriy-kudrya@users.noreply.github.com>
2022-04-17 23:33:36 +0300

    switching subscription hook implementation to useSyncExternalStore

--- app/utils/react/use-subscription.ts
+++ app/utils/react/use-subscription.ts
@@ -1,8 +1,6 @@
-import { useEffect, useRef } from 'react'
-import useForceUpdate from './use-force-update'
+import { useCallback, useRef, useSyncExternalStore } from 'react'
 
-const strictEqual = <T>(one: T, two: T) => one === two
-    , getValue = <T, M = T>(get: () => T, map?: (value: T) => M): M => map ? map(get()) : get as any
+const getValue = <T, M = T>(get: () => T, map?: (value: T) => M): M => map ? map(get()) : get() as any
 
 /**
  * Tracks changes to subscribed value
@@ -28,65 +26,85 @@ function useSubscription<T, M = T>(
 function useSubscription<T, M = T>(
     subscribe: (callback: () => void) => () => void,
     getState: () => T,
-    equal: (one: any, two: any) => boolean = strictEqual,
+    equal: (one: any, two: any) => boolean = Object.is,
     mapState?: (value: T) => M
 ): M {
-    type State = {
-        currentValue: T | M
-        getState: typeof getState
-        mapState: typeof mapState
-        equal: typeof equal
-    }
+    const prevValue = useRef<{ value: M }>()
 
-    const forceUpdate = useForceUpdate()
-        , stateRef = useRef<State>()
-        , state = stateRef.current
-        , uncommittedValue = state?.getState === getState && state.mapState === mapState
-            ? state.currentValue
-            : getValue(getState, mapState)
-
-    useEffect(
+    const getSnapshot = useCallback(
         () => {
-            stateRef.current = {
-                currentValue: uncommittedValue,
-                getState,
-                mapState,
-                equal,
-            }
-        }
-    )
+            const value = getValue(getState, mapState)
 
-    useEffect(
-        () => {
-            let disposed = false
+            if (!prevValue.current)
+                prevValue.current = { value }
 
-            function handleChange() {
-                if (disposed)
-                    return
+            if (equal(value, prevValue.current.value))
+                return prevValue.current.value
 
-                const state = stateRef.current!
-                    , nextValue = getValue(state.getState, state.mapState)
+            prevValue.current.value = value
 
-                if (state.equal(nextValue, state.currentValue))
-                    return
-
-                state.currentValue = nextValue
-                forceUpdate()
-            }
-
-            handleChange()
-
-            const dispose = subscribe(handleChange)
-            return () => {
-                disposed = true
-                dispose()
-            }
+            return value
         },
-        // no need to include 'forceUpdate' dependency but eslint is complaining
-        [subscribe, forceUpdate]
+        [getState, mapState, equal]
     )
 
-    return uncommittedValue as any
+    return useSyncExternalStore(subscribe, getSnapshot)
+    // type State = {
+    //     currentValue: T | M
+    //     getState: typeof getState
+    //     mapState: typeof mapState
+    //     equal: typeof equal
+    // }
+
+    // const forceUpdate = useForceUpdate()
+    //     , stateRef = useRef<State>()
+    //     , state = stateRef.current
+    //     , uncommittedValue = state?.getState === getState && state.mapState === mapState
+    //         ? state.currentValue
+    //         : getValue(getState, mapState)
+
+    // useEffect(
+    //     () => {
+    //         stateRef.current = {
+    //             currentValue: uncommittedValue,
+    //             getState,
+    //             mapState,
+    //             equal,
+    //         }
+    //     }
+    // )
+
+    // useEffect(
+    //     () => {
+    //         let disposed = false
+
+    //         function handleChange() {
+    //             if (disposed)
+    //                 return
+
+    //             const state = stateRef.current!
+    //                 , nextValue = getValue(state.getState, state.mapState)
+
+    //             if (state.equal(nextValue, state.currentValue))
+    //                 return
+
+    //             state.currentValue = nextValue
+    //             forceUpdate()
+    //         }
+
+    //         handleChange()
+
+    //         const dispose = subscribe(handleChange)
+    //         return () => {
+    //             disposed = true
+    //             dispose()
+    //         }
+    //     },
+    //     // no need to include 'forceUpdate' dependency but eslint is complaining
+    //     [subscribe, forceUpdate]
+    // )
+
+    // return uncommittedValue as any
 }
 
 export default useSubscription
